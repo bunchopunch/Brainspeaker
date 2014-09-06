fb71ae056c9b7053dd8168551b253d32e307230b
bunchopunch <bunchopunch@mustacheforpresident.com>
2014-09-06 17:40:14 -0600

    braking stuff and adding levels

--- dist/js/game.js
+++ dist/js/game.js
@@ -107,7 +107,10 @@ module.exports = Menu;
     rightKey: null,
     gob: null,
     anim: null,
-    currentLevel: 0,
+    state: {
+      step: 0,
+      level: 0,
+    },
     levels: [
       ["u", "r", "l", "u"],
       ["u", "u", "r", "r", "l", "l"],
@@ -122,6 +125,7 @@ module.exports = Menu;
       this.downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
       this.leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
       this.rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
 
       this.appendMove = function(keyEvent){
         var toAppend = null,
@@ -148,10 +152,6 @@ module.exports = Menu;
       }
 
 
-      this.initLevel = function(){
-        this.levels[this.currentLevel].forEach(this.simonSays);
-      }
-
       this.simonSays = function(direction){
         switch (direction){
           case "u":
@@ -173,11 +173,50 @@ module.exports = Menu;
       }
 
 
+      // ========================================================
+
       var gob = this.game.add.sprite(this.game.world.centerX - (250*0.8) , this.game.world.centerY - (380*0.8), 'gob');
       gob.scale.setTo(.8, .8);
       gob.animations.add('left', [0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0], true);
       gob.animations.add('right', [0, 10, 9, 8, 7, 6, 7, 8, 9, 10, 0], true);
       gob.animations.add('up', [0, 11, 12, 13, 14, 15, 14, 13, 12, 11, 0], true);
+
+      ASDASF
+      // THIS IS THE PROBLEM. Why can state get state, but levelz can't get levelz?!??!?!?!?
+      var state = this.game.state;
+      var levelz = this.game.levels;
+      console.log(levelz);
+
+      var letsTryThisAgain = function(){
+        console.log(levelz);
+        if (levelz.length < state.step) {
+          state.step = 0;
+          return
+        };
+
+//        console.log(this.levels[this.currentLevel][this.currentStep]);
+        this.currentStep++;
+                //        for (var i = this.levels.length - 1; i >= 0; i--) {
+                //          console.log(this.levels[i]);
+                //        };
+      }
+
+      gob.events.onAnimationComplete(letsTryThisAgain(), this);
+
+
+
+                // Track state (current step in level's command list / current level)
+                // The initial function and the onAnimationComplete are prob the same function
+                // if step > length return
+
+
+      this.initLevel = function(){
+//        this.levels[this.currentLevel].forEach(this.simonSays);
+
+        letsTryThisAgain();
+
+      }
+
 
     },
     update: function() {
@@ -187,9 +226,10 @@ module.exports = Menu;
       this.rightKey.onDown.add(this.appendMove, this);
       this.leftKey.onDown.add(this.appendMove, this);
 
-      this.initLevel();
+      this.initLevel(0);
 
     },
+    
 //      this.gob.play("left", 20, true);
   };
   
